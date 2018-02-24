cb0be13075d50fb05b99c289290b816ae10386f8
kamalkrishna3000 <kamalkrishna3000@gmail.com>
2018-02-24 12:53:52 +0530

    Session2 changes.

---
+++ React Native/training/index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+//import App from './App';
+import App from './startUp.js';
+
+//import App from './App2.js';
+AppRegistry.registerComponent('training', () => App);
